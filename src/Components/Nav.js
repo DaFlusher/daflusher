964050176050d9906ecee1eda8b9958722c333b6
DaFlusher <joyibe5@gmail.com>
2023-09-14 19:21:22 +0100

    Adding Bootstrap cdn files and installing via npm install react-bootstrap bootstrap

---
+++ src/Components/Nav.js
@@ -0,0 +1,12 @@
+const Nav = () => {
+    return (
+        <ul>
+            <li>Home</li>
+            <button className="btn btn-secondary">Click me</button>
+
+        </ul>
+
+      );
+}
+ 
+export default Nav;
