aaac5258997de61b1f567f20eb580e09861afa7a
DaFlusher <joyibe5@gmail.com>
2023-09-18 20:09:43 +0100

    Adding the About and contact page together

--- src/Components/About.js
+++ src/Components/About.js
@@ -4,8 +4,34 @@ const About = () => {
     return (  
         <div>
             <Nav/>
+                <div className="about-grid">
+                    <section className="contact">
+                        <h1>Contact</h1>
+                        
+                        <img src={require('./daflusher.jpg')} alt="Joy Ibe" style={{width:'50%', margin:'0 auto',borderRadius:'50%'}}/>
+                        
+                        <div className="contact-text">
+                            <div className="inline-title">
+                                <h3 className="contact-subtext">Name: </h3>
+                                <h3 className="contact-subtext">Alias: </h3>
+                            </div>
+                            <div className="inline-text">
+                                <h3 className="contact-subtext"> Joy Chidinma Ibe</h3>
+                                <h3 className="contact-subtext"> DaFlusher</h3>
+                            </div>
 
-                <h1>About me</h1>
+                            
+                        </div>
+                        
+                        <button className="resume" type="button" name="resume">Download Resume</button>
+                    
+                    </section>
+
+                    <section className="about"> 
+                        <h1>About me</h1>
+                    </section>
+                </div>
+                
             <Footer/>
         </div>
     );
